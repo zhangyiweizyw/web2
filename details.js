8e15a83038644b5e278e9edcf8754ba7d9e36028
zhangyiweizyw <520071230@qq.com>
2019-01-06 13:47:33 +0800

    homework

---
+++ details.js
@@ -0,0 +1,142 @@
+//图片选择
+var sp0 = document.getElementById('sp0');
+var sp1 = document.getElementById('sp1');
+var p0 = document.getElementById('p0');
+sp1.onmouseover = function () {
+	this.style.border = "orange solid 2px";
+	p0.style.background = "url(img/pp1.jpeg) center left";
+	sp0.style.border = "none";
+}
+// sp1.onmouseout = function () {
+// 	this.style.border = "none";
+// }
+sp0.onmouseover = function () {	
+	this.style.border = "orange solid 2px";
+	p0.style.background = "url(img/pp0.jpeg) center left";
+	sp1.style.border = "none";
+}
+// sp0.onmouseout = function () {
+// 	this.style.border = "none";
+// }
+//购买数量选择
+var choose = document.getElementById('choose');
+var token = document.getElementsByClassName('token');
+token[1].onclick = function () {	
+	if(num.value < 5){
+		num.value++;
+	}
+}
+token[0].onclick = function () {	
+	if(num.value > 1){
+		num.value--;
+	}
+}
+token[0].onmouseover = function () {
+	if(num.value ==1){
+		this.style = "cursor:not-allowed";
+	}
+	else{
+		this.style = "cursor:pointer";
+	}
+}
+token[1].onmouseover = function () {
+	if(num.value == 5){
+		this.style = "cursor:not-allowed";
+	}
+	else{
+		this.style = "cursor:pointer";
+	}
+}
+
+//容量选择
+var redBorder = document.getElementsByClassName('redBorder');
+var capacity = document.getElementById('capacity');
+redBorder[0].onclick = function () {
+	this.style.border = "red solid 1px";
+	this.style.background = "url(img/duigou.png) bottom right no-repeat";
+	capacity.innerHTML = "\'"+this.innerHTML+"\'";
+	redBorder[1].style.border = "none";
+	redBorder[1].style.background = "none";
+
+}	
+redBorder[1].onclick = function () {
+	this.style.border = "red solid 1px";
+	this.style.background = "url(img/duigou.png) bottom right no-repeat";
+	capacity.innerHTML = "\'"+this.innerHTML+"\'";
+	redBorder[0].style.border = "none";
+	redBorder[0].style.background = "none";
+}	
+	
+
+//点击加入购物车
+var button2 = document.getElementById('button2');
+var alertBox = document.getElementById('alertBox');
+var innerbox = document.getElementById('innerbox');
+var close = document.getElementById('close');
+button2.onclick = function () {
+	alertBox.style.display = 'block';
+	innerbox.style.display = 'block';
+}
+close.onclick = function () {
+	alertBox.style.display = 'none';
+	innerbox.style.display = 'none';
+}
+
+
+//放大镜效果
+var p0 = document.getElementById('p0');
+var smallSlider = document.getElementById('smallSlider');
+var bigPp0 = document.getElementById('bigPp0');
+var bigPp1 = document.getElementById('bigPp1');
+var bigP0 = document.getElementById('bigP0');
+var background1 = 'url("img/pp0.jpeg") left center';
+var background2 = 'url("img/pp1.jpeg") left center';
+p0.onmouseover = function () {
+	bigP0.style.display = 'block';
+	bigPp0.style.display = 'block';
+  	smallSlider.style.display = "block";
+  	if(p0.style.background == background2){  		
+  		bigPp1.style.display = "block";
+  		bigPp0.style.display = "none";
+  	}
+  	else{
+  		bigPp0.style.display = "block";
+  		bigPp1.style.display = "none";
+  	}
+
+}
+p0.onmouseout = function () {
+	bigP0.style.display = 'none';
+	smallSlider.style.display = "none";
+}
+
+p0.onmousemove = function (e) {
+	var e = e || window.event;
+	var left = e.clientX - 89 - smallSlider.offsetWidth/2;  //画图找到要求的位置,鼠标当前距离浏览器的距离-box距离浏览器的距离-放大镜的一半，从左上角入手
+	var top = e.clientY - 130- smallSlider.offsetHeight/2;
+	var maxLeft = p0.offsetWidth - smallSlider.offsetWidth;
+ 	var maxTop = p0.offsetHeight - smallSlider.offsetHeight;
+	left = left > maxLeft?maxLeft:left<0?0:left;  //如果left超出右边界则返回max以保证max <= max ,max >=0
+	top = top > maxTop? maxTop: top<0?0:top;
+	//2.设置放大镜的位置
+	smallSlider.style.left =  left + "px"; //由于style.left可读可写，offsetLeft只读，但是style.left设置时要有单位
+	smallSlider.style.top = top + "px";
+	//3.根据左侧放大镜位置，计算右侧大图的移动比例
+	var w = left / maxLeft;  //当前位置/极限距离  -- 计算左侧放大镜移动的比例
+	var h = top / maxTop;  //通过此比例联系左右
+	//计算大图的最大移动范围
+	if(bigPp1.style.display == 'block'){		
+	  	var BimgLeft = bigP0.offsetWidth/2 - bigPp1.offsetWidth;
+		var BimgTop = bigP0.offsetHeight/2 - bigPp1.offsetHeight;
+		//计算大图的移动距离，并设置位置
+		bigPp1.style.left = w*BimgLeft + "px";
+		bigPp1.style.top = h*BimgTop + "px";
+	}
+	else{
+		var BimgLeft = bigP0.offsetWidth - bigPp0.offsetWidth;
+		var BimgTop = bigP0.offsetHeight - bigPp0.offsetHeight;
+		//计算大图的移动距离，并设置位置
+	  	bigPp0.style.left = w*BimgLeft + "px";
+	  	bigPp0.style.top = h*BimgTop + "px";
+	}
+}
---
+++ index.js
@@ -0,0 +1,139 @@
+window.onload = function() {
+	var cover = document.getElementsByClassName('cover')[0];
+	window.onscroll = function() {
+		var st = document.documentElement.scrollTop || document.body.scrollTop;
+		if(st > 152){
+			cover.style.position = 'fixed';
+		}else{
+			cover.style.position = 'static';
+		}
+	}
+}
+//充值显示
+var select = document.getElementById('select');
+var button = document.getElementsByClassName('button');
+var money = document.getElementsByClassName('money');
+button[0].onclick = function () {
+money[1].innerHTML = "￥"+select.value;
+}
+//轮播图
+var banner = document.getElementById('banner');
+var oNavlist = document.getElementById('dot').children;
+var slider = document.getElementById('slider');
+var left = document.getElementById('leftarrow');
+var right = document.getElementById('rightarrow');
+var index = 1;
+var timer;
+var isMoving = false;
+banner.onmouseover = function(){
+	animate(left,{opacity:50});
+	animate(right,{opacity:50});
+	clearInterval(timer);
+}
+banner.onmouseout = function(){
+	animate(left,{opacity:0})
+	animate(right,{opacity:0})
+	timer = setInterval(next,3000);
+}
+right.onclick = next;
+left.onclick = prev;
+for( var i=0; i<oNavlist.length; i++ ){
+	(function(i){
+		oNavlist[i].onclick = function(){
+			index = i+1;
+			navmove();
+			animate(slider,{left:-800*index});
+		}
+	})(i);
+}
+function next(){
+	if(isMoving){
+		return;
+	}
+	isMoving = true;
+	index++;
+	navmove();
+	animate(slider,{left:-800*index},function(){
+		if(index==7){
+			slider.style.left = '-800px';
+			index = 1;
+		}
+		isMoving = false;
+	});
+}
+function prev(){
+	if(isMoving){
+		return;
+	}
+	isMoving = true;
+	index--;
+	navmove();
+	animate(slider,{left:-800*index},function(){
+		if(index==0){
+			slider.style.left = '-5600px';
+			index = 6;
+		}
+		isMoving = false;
+	});
+}
+function navmove(){
+	for( var i=0; i<oNavlist.length; i++ ){
+		oNavlist[i].className = "";
+	}
+	if(index > 6){
+		oNavlist[0].className = "active";
+	}else if(index<=0){
+		oNavlist[5].className = "active";
+	}else {
+		oNavlist[index-1].className = "active";
+	}
+}
+timer = setInterval(next,3000);
+
+//右侧公告滚动
+var index1 = 0;
+var isMoving1 = false;
+var glide = document.getElementById('glide');
+var notice = document.getElementById('notice');
+function glideNotice(){
+	if(isMoving1){
+		return;
+	}
+	isMoving1 = true;
+	index1++;
+	animate(glide,{top:-21*index1},function(){
+		if(index1 == 14){
+			glide.style.top = '0';
+			index1 = 1;
+		}
+		isMoving1 = false;
+	});
+}		
+var timer1 = setInterval(glideNotice,1000);
+notice.onmouseover = function () {
+	clearInterval(timer1);
+}
+notice.onmouseout = function () {
+	timer1 = setInterval(glideNotice,1000);
+}
+
+//右侧图标
+var smallBox = document.getElementById('smallBox');
+var li = smallBox.children;
+var part = document.getElementsByClassName('part');
+// var part = document.getElementsByClassName('part');
+for( var i=0; i < part.length; i++ ){
+	(function(i){
+		part[i].onmouseover = function(){
+			// this.style.marginRight = '0';
+			animate(this,{right:60});
+		}
+	})(i);
+}
+for( var i=0; i < part.length; i++ ){
+	(function(i){
+		part[i].onmouseout = function(){
+			animate(this,{right:0});
+		}
+	})(i);
+}
